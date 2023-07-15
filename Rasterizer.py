3621571cdb5abf44bc1d29b3176e43f2958ff360
Andrés Montoya <montoyaw1@gmail.com>
2023-07-15 02:48:14 -0600

    Resolution change
    Change to 1920 x 1080

--- Rasterizer.py
+++ Rasterizer.py
@@ -1,7 +1,7 @@
 from gl import Renderer, V2,V3, color
 import random, shaders
-width = 500
-height = 500
+width = 1920
+height = 1080
 
 rend = Renderer(width, height)
 
@@ -39,7 +39,7 @@ rend.fragmentShader=shaders.fragmentShader
 #Right: -90,0,90
 #Front: -90,0,0
 #Left: -90,0,-90
-rend.glLoadModel("skull.obj", translate=(width/2, height/8, 0), scale=(15,15,15), rotate=(-90,0,-90))
+rend.glLoadModel("skull.obj", translate=(width/2, height/8, 0), scale=(30,30,30), rotate=(-90,0,90))
 
 """ verts = [V3(0,0,0),
         V3(50,0,0),
@@ -50,4 +50,4 @@ rend.glModelMatrix(translate=(width/2, height/2, 0),
                    scale=(5,3,1)) """
 rend.glRender()
 
-rend.glFinish("Left.bmp") 
+rend.glFinish("Right.bmp") 
