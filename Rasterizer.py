e12d74ca4a0f9f0d7d7369ce593b71b76e789523
Andrés Montoya <montoyaw1@gmail.com>
2023-08-05 14:59:00 -0600

    cone

--- Rasterizer.py
+++ Rasterizer.py
@@ -8,21 +8,23 @@ rend = Renderer(width, height)
 rend.vertexShader=shaders.vertexShader
 rend.fragmentShader=shaders.fragmentShader
 
-rend.glLookAt(camPos=(-3,10,-2),
-              eyePos=(0,0,-5))
+#rend.glLookAt(camPos=(0,-3,0),
+#             eyePos=(0,-2,-3))
+rend.glCameraMatrix(translate=(0,-2,0),
+                    rotate=(0,0,15))
 
 #Right: -90,0,90
 #Back: -90,0,0
 #Front: -90,0,-180
 #Left: -90,0,-90
 
-rend.glLoadModel(filename="./models/face/object.obj", 
-                 textureName="./models/face/texture.bmp", 
-                 translate=(0, 0, -5), 
-                 scale=(1.5,1.5,1.5), 
+rend.glLoadModel(filename="./models/cone/object.obj", 
+                 textureName="./models/cone/texture.bmp", 
+                 translate=(0, -3, -3), 
+                 scale=(4,4,4), 
                  rotate=(0,0,0))
 
 
 rend.glRender() 
 
-rend.glFinish("./renders/face/FaceCams.bmp") 
+rend.glFinish("./renders/cone/DutchAngle.bmp") 
